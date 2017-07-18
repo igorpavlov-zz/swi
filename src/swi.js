bebeb3958594f8e7689565e25c5fb7f32b795102
Igor Pavlov <pavlov.org@gmail.com>
2017-07-18 01:49:31 +0100

    Code styling

--- src/swi.js
+++ src/swi.js
@@ -13,17 +13,19 @@
 }(this, 'swi', function (options) {
   return function (options) {
     options = options || {};
+
     options.element = options.element || document;
 
+    var lock;
     var mwX1 = 0;
     var mwX2 = 0;
     var mwY1 = 0;
     var mwY2 = 0;
-    var lock;
 
     function onScroll (e) {
       var deltaX = e.deltaX ? e.deltaX : e.detail;
       var deltaY = e.deltaY ? e.deltaY : e.detail;
+
       if (
         (
           (
@@ -40,35 +42,31 @@
         !lock
       ) {
         lock = true;
+
         window.setTimeout(function () {
           lock = false;
         }, 500);
+
         if (Math.abs(deltaX) >= Math.abs(deltaY)) {
           if (deltaX < 0) {
-            if (options.right) {
-              options.right();
-            }
+            if (options.right) options.right();
           } else {
-            if (options.left) {
-              options.left();
-            }
+            if (options.left) options.left();
           }
         } else {
           if (deltaY < 0) {
-            if (options.down) {
-              options.down();
-            }
+            if (options.down) options.down();
           } else {
-            if (options.up) {
-              options.up();
-            }
+            if (options.up) options.up();
           }
         }
       }
+      
       mwX1 = mwX2;
       mwX2 = deltaX;
       mwY1 = mwY2;
       mwY2 = deltaY;
+
       e.preventDefault();
       e.stopPropagation();
     }
